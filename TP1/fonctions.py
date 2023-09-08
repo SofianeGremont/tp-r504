cbc8c61267094a31aeed8e074922ce5a6dcffa27
gremosof <sofianegremont12@gmail.com>
2023-09-08 14:36:28 +0200

    mise à jour

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -4,8 +4,6 @@ def puissance(a, b):
   elif not type(b) is int:
     raise TypeError("Only integers are allowed")
   else:
-    if b < 0:
-      raise ValueError("L'exposant doit être positif ou nul.")
     if a < 0:
       resultat = -1
     resultat = 1
@@ -15,4 +13,6 @@ def puissance(a, b):
       resultat = -1
     if a == 0 and b == 0:
       resultat = 1
+    if b < 0:
+      resultat = 0
     return resultat
