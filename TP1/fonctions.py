d1b2f99b01c39be6170f76bea18e2f09a1b2c73c
gremosof <sofianegremont12@gmail.com>
2023-09-08 14:31:20 +0200

    mise à jour

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -4,5 +4,15 @@ def puissance(a, b):
   elif not type(b) is int:
     raise TypeError("Only integers are allowed")
   else:
-    resultat = a ** b
+    if b < 0:
+      raise ValueError("L'exposant doit être positif ou nul.")
+    if a < 0:
+      resultat = -1
+    resultat = 1
+    for _ in range(b):
+      resultat *= a
+    if a < 0 and b == 0:
+      resultat = -1
+    if a == 0 and b == 0:
+      resultat = 1
     return resultat
--- TP1/test.py
+++ TP1/test.py
@@ -2,12 +2,23 @@ import pytest
 import fonctions as f
 
 def test_1 ():
-  assert f.puissance(2, 3) == 8
-  assert f.puissance(2, 2) == 4
+	assert f.puissance(2, 3) == 8
+	assert f.puissance(2, 2) == 4
 
 def test_2 ():
-  assert f.puissance(-2, 3) == -8
-  assert f.puissance(-5, 5) == -3125
+	assert f.puissance(-2, 3) == -8
+	assert f.puissance(-5, 5) == -3125
+
+def test_3 ():
+	assert f.puissance (2, 2) == 4
+	assert f.puissance (-2, 2) == 4
+	assert f.puissance (2, 0) == 1
+	assert f.puissance (-2, 0) == -1
+	assert f.puissance (0, 0) == 1
+	assert f.puissance (0, 3) == 0
+	assert f.puissance (0, -3) == 0
+
 
 test_1 ()
 test_2 ()
+test_3 ()
